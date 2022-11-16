00f5b72929738475b8e2b7f5bcb06ce071a34897
pgshehata <pgshehata@gmail.com>
2022-11-16 12:24:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 self.addEventListener('push', function(event) {
-    console.log('Received a push message', event);
+    var event_obj = event;
+    
+    console.log('Received a push message', event_obj);
 
     var title = 'Yay a message.';
     var body = 'We have received a push message.';

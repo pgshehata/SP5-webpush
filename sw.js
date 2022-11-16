3b40ddfa93d4e7f84b583cd763ce99d55326afef
pgshehata <pgshehata@gmail.com>
2022-11-16 14:32:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
 self.addEventListener('push', function(event) {
-    if (event.data) {
-    console.log('Push event!! ', event.data.text())
-  } else {
-    console.log('Push event but no data')
-  }
-
     var title = 'La Banque des Territoires';
-    var body = "Une nouvelle actualité vient d'être publiée";
+    var body = event.data.text();
     var icon = 'https://www.banquedesterritoires.fr/sites/default/files/favicons/favicon-32x32.png';
     var tag = 'simple-push-demo-notification-tag';
     var data = {

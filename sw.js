d0ccac9d683a3b58c743df0bdf35a959c6e3ee70
pgshehata <pgshehata@gmail.com>
2022-11-16 12:46:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,22 +3,27 @@ self.addEventListener('push', function(event) {
     
     console.log('Received a push message', event_obj.data.text);
 
-    var title = 'Yay a message.';
-    var body = 'We have received a push message.';
-    var icon = '/images/icon-192x192.png';
+    var title = 'La Banque des Territoires';
+    var body = "Une nouvelle actualité vient d'être publiée";
+    var icon = 'https://www.banquedesterritoires.fr/sites/default/files/favicons/favicon-32x32.png';
     var tag = 'simple-push-demo-notification-tag';
     var data = {
         doge: {
             wow: 'such amaze notification data'
         }
     };
+    var actions = [
+        { action: 'explore', title: "Voir l'actualité", icon: ''},
+        { action: 'close', title: "Fermer", icon: ''},
+        ];
 
     event.waitUntil(
         self.registration.showNotification(title, {
             body: body,
             icon: icon,
             tag: tag,
-            data: data
+            data: data,
+            actions: actions
         })
     );
 });

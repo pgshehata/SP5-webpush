62c5f181775b1a140064dfe6f13be6fd2cafc8dd
pgshehata <pgshehata@gmail.com>
2022-11-16 10:37:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', options);
+self.addEventListener('push', function(event) {
+    console.log('Received a push message', event);
+
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = '/images/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
+    var data = {
+        doge: {
+            wow: 'such amaze notification data'
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag,
+            data: data
+        })
+    );
 });

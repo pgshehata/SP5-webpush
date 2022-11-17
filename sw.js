3c230cd852dab1ada5a14a6fcfb1b26d81d11bce
pgshehata <pgshehata@gmail.com>
2022-11-17 16:47:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,32 @@
 self.addEventListener('push', function(event) {
 
     var title = 'La Banque des Territoires';
-    var body = "Une nouvelle actualité vient d'être publiée";
+    var body = event.data.text();
     var icon = 'https://www.banquedesterritoires.fr/sites/default/files/favicons/favicon-32x32.png';
-    var tag = 'simple-push-demo-notification-tag';
-    var data = {
-        doge: {
-            wow: 'such amaze notification data'
-        }
-    };
+    var tag = 'news-notification-tag';
+
     var actions = [
         { action: 'explore', title: "Voir l'actualité", icon: '' },
         { action: 'close', title: "Fermer", icon: '' },
     ];
 
-    if (Notification.permission === "granted") {
-        const notificationText = event.data.text();
-        const showNotification = self.registration.showNotification('La Banque des Territoires', {
-            body: notificationText,
-            icon: 'https://www.banquedesterritoires.fr/sites/default/files/favicons/favicon-32x32.png'
-        });
-        // Make sure the toast notification is displayed.
-        event.waitUntil(showNotification);
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag,
+            actions: actions
+        })
+    );
+});
+
+self.addEventListener('notificationclick', (event) => {
+    switch (event.action) {
+        case 'close':
+            event.notification.close();
+            break;
+        default:
+            console.log(`Unknown action clicked: '${event.action}'`);
+            break;
     }
-    /*
-        event.waitUntil(
-            self.registration.showNotification(title, {
-                body: body,
-                icon: icon,
-                tag: tag,
-                data: data,
-                actions: actions
-            })
-        );
-        */
 });
